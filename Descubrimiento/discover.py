0aee26aeca832556c5091e4a39527ec3006d85c0
David Ferreira <ferreiradavid0212@gmail.com>
2024-04-21 23:14:10 -0600

    Optener las interfaces y las ips

--- Descubrimiento/discover.py
+++ Descubrimiento/discover.py
@@ -1,8 +1,6 @@
 import netmiko as nm
 import functions
 
-ips = []
-current_device = 0
 
 def set_next_connection(ip):
     router = {
@@ -21,8 +19,8 @@ def set_next_connection(ip):
 def find_neighbours(conn):
     _cdp = conn.send_command("show cdp n d")
 
-    n_ips = functions.get_ip(_cdp)
-    n_interfaces = functions.get_interfaces(_cdp)
+    n_ips = functions.get_ips(_cdp)
+    n_inter = functions.get_interfaces(_cdp)
 
     #for i in n_ips:
     #    if i not in ips:
@@ -30,9 +28,13 @@ def find_neighbours(conn):
     #        set_next_connection(i)
 
 def query_info(conn):
-    pass
+    _interfaces = conn.send_command("show ip int brief")
+    n_interfaces = functions.get_interfaces(info)
 
+ips = []
+current_device = 0
 devices = dict()
+
 set_next_connection('10.10.69.1')
 
 print(devices)
--- Descubrimiento/functions.py
+++ Descubrimiento/functions.py
@@ -1,8 +1,8 @@
 
 from re import finditer
+import netmiko as nm
 
-
-def get_ip(output):
+def get_ips(output):
     a = [m.start() for m in finditer('IP', output)]
     ips = []
     for i in a:
@@ -22,3 +22,38 @@ def get_interfaces(output):
         ints.append(temp)
     
     return ints
+
+def active_interfaces(output):
+    #faltan cosas
+    #get interface
+    temp = output[output.find('\n'):]
+    print(temp)
+    interfaces = [m.start() for m in finditer('\n', temp)]
+    lines = list()
+
+    for i in range(len(interfaces)):
+        lines.append(temp[interfaces[i+1] : interfaces[i-1]])
+
+
+    print(lines)
+
+    
+    
+
+
+router = {
+    'device_type': 'cisco_ios',
+    'ip': '10.10.69.1',
+    'username': 'test',
+    'password': 'test',
+    'secret': 'test',
+    'port': 22,
+    }
+
+conn = nm.ConnectHandler(**router)
+
+intb = conn.send_command('show ip int brief')
+
+
+active_interfaces(intb)
+#print(intb)
