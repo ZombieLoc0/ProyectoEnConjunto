e941b875993769ef36016f6ace058233fbd8c2c5
Carlos Delgadillo <carlos.delgadillo102003@gmail.com>
2024-04-29 14:37:29 -0600

    Union json a la API
    Agregue test.js que manda los datos a la APi server.js y este cambia el archivo data.json local para asi mostrar la topologia

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
 const path = require('path');
+var fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(cors());
+app.use(express.json()); // Middleware para analizar el cuerpo de la solicitud como JSON
+
 
 // Ruta para servir el archivo data.json
 app.get('/data', (req, res) => {
@@ -27,6 +29,27 @@ app.get('/data', (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+
+// Ruta para recibir un JSON mediante POST y enviarlo por GET
+app.post('/update-data', (req, res) => {
+    try {
+        var receivedData = req.body; // Datos recibidos en el cuerpo de la solicitud POST
+        console.log('Datos recibidos:', receivedData);
+
+        // Escribir los datos recibidos en el archivo data.json
+        const dataPath = path.join(__dirname, '/data.json');
+        fs.writeFileSync(dataPath, JSON.stringify(receivedData, null, 4));
+
+        // Envía los datos recibidos como respuesta a través de la ruta GET /data
+        res.redirect(303, '/data');
+    } catch (error) {
+        console.error('Error al escribir en data.json:', error);
+        res.status(400).json({ error: 'Error al escribir en data.json' });
+    }
+});
+
+
 
 // Función para eliminar enlaces duplicados
 function removeDuplicateLinks(links) {
@@ -57,4 +80,47 @@ function normalizeLink(link) {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Codigo mensajes Twilio
+// Importa el módulo Twilio para interactuar con su API
+const twilio = require('twilio');
+
+// Credenciales de autenticación de Twilio
+const accountSid = 'AC197950e7e248ff96d25f214b519939f0'; // Account SID
+const authToken = 'b992c9a07521c87727fc84b3474c7142';     // Auth Token
+
+// Inicializa el cliente de Twilio con las credenciales proporcionadas
+const client = twilio(accountSid, authToken);
+
+// Utiliza el cliente de Twilio para enviar un mensaje SMS
+client.messages
+  .create({
+    // Cuerpo del mensaje SMS
+    body: 'The fucking best team enviado desde Twilio.',
+    // Número de teléfono desde el cual se enviará el mensaje (tu número de Twilio)
+    from: '+14015194259',
+    // Número de teléfono al cual se enviará el mensaje (el destinatario)
+    to: '+523334707958'
+  })
+  .then(message => console.log(message.sid)) // Maneja la respuesta exitosa imprimiendo el SID del mensaje
+  .catch(error => console.error('Error al enviar el mensaje:', error)); // Maneja cualquier error durante el envío del mensaje
+
 
---
+++ test.js
@@ -0,0 +1,26 @@
+
+var request = require('request');
+
+var linkD= {
+    "nodes": [
+      { "key": "192.168.10.2", "type": "Router" },
+      { "key": "192.168.10.3", "type": "Switch" },
+      { "key": "192.168.10.4", "type": "Switch" }
+    ],
+    "links": [
+      { "from": "192.168.10.2", "to": "192.168.10.3","t1":"G0/0","t2":"G0/0" },
+      { "from": "192.168.10.3", "to": "192.168.10.2","t1":"G0/0","t2":"G0/0" },
+      { "from": "192.168.10.3", "to": "192.168.10.4","t1":"G/0/1","t2":"G0/1"  },
+      { "from": "192.168.10.4", "to": "192.168.10.2","t1":"G0/2","t2":"G0/1"  },
+      { "from": "192.168.10.2", "to": "192.168.10.4","t1":"G0/1","t2":"G0/2"  }
+    ]
+}
+
+
+request({
+    url:"http://localhost:3000/update-data",
+    method: "POST",
+    json: true,
+    body: linkD,
+}, function(error,response,body){console.log(response)}
+);
