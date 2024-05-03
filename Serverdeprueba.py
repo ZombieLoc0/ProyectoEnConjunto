1df2c44fba926b92f02c1869771c5a0d89f71502
Sebastian Diaz1 <cesar.mercado@edu.uag.mx>
2024-05-03 08:56:44 -0600

    Update Serverdeprueba.py

--- Serverdeprueba.py
+++ Serverdeprueba.py
@@ -2,33 +2,21 @@ from flask import request, Flask, jsonify
 from flask_cors import CORS
 
 server = Flask(__name__)
+
+# Habilitar el manejo de solicitudes CORS para permitir solicitudes desde cualquier origen
 CORS(server)
-
-def generate_commands(config_data):
-    commands = []
-
-    if 'hostname' in config_data:
-        commands.append("enable\nconfigure terminal\n" + f"hostname {config_data['hostname']}\nexit")
-    if 'motd' in config_data:
-        commands.append("enable\nconfigure terminal\n" + f"banner motd {config_data['motd']}\nexit")
-
-    return commands
-
 
 @server.route("/set-connection", methods=["POST"])
 def query_and_discovery():
     conn = request.get_json()
-    print(conn)
+    print(conn) 
     return jsonify({"message": "Conexión establecida correctamente"}), 200
 
 @server.route("/send-config", methods=["POST"])
 def set_configuration():
-    new_config = request.get_json()
-    commands = generate_commands(new_config)
-    print("Comandos de configuración generados:", commands)
-    # Aquí deberías implementar la lógica para enviar los comandos a los dispositivos
-    # Por ahora, simplemente los imprimimos en la consola del servidor
+    newConfig = request.get_json()
+    print(newConfig)  # Imprimir los datos JSON en la consola del servidor
     return jsonify({"message": "Configuración recibida correctamente"}), 200
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     server.run(debug=True)
