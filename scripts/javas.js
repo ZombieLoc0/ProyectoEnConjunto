3aa945bd095feb5acb0ca1bf0b603d059a60f26a
Sebastian Diaz1 <cesar.mercado@edu.uag.mx>
2024-05-08 12:17:48 -0600

    Cerrar configs

--- scripts/javas.js
+++ scripts/javas.js
@@ -176,35 +176,16 @@ function toggleDHCPv6Fields() {
     }
 }
 
+function closeDeviceInfo() {
+    document.getElementById("deviceInfo").style.display = "none";
+}
 
-const mapContainer = document.querySelector('.map-container');
-const mapImage = document.getElementById('mapImage');
+function closeRouterConfig() {
+    document.getElementById("routerConfig").style.display = "none";
+}
 
-let isDragging = false;
-let startX, startY, offsetX = 0, offsetY = 0;
-
-mapImage.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.clientX - mapImage.offsetLeft;
-    startY = e.clientY - mapImage.offsetTop;
-});
-
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-});
-
-mapContainer.addEventListener('mousemove', (e) => {
-    if (isDragging) {
-        e.preventDefault();
-        let x = e.clientX - startX;
-        let y = e.clientY - startY;
-        setPosition(x, y);
-    }
-});
-
-function setPosition(x, y) {
-    mapImage.style.left = x + 'px';
-    mapImage.style.top = y + 'px';
+function closeSwitchConfig() {
+    document.getElementById("switchConfig").style.display = "none";
 }
 
 function openTab(evt, tabName) {
